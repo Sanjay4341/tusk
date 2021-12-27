3d44b7f0561ca8b8a128b5a36e4c896a51eaf55c
Sanjay4341 <85867127+Sanjay4341@users.noreply.github.com>
2021-12-28 00:10:08 +0530

    Arthimatic file created

---
+++ Arthimatic.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Arthimatic
+{
+	public static void main(String[] args)
+	{
+		int c,d,e,f;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter values of a and b : ");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		c=a+b;
+		System.out.println("Sum is "+c);
+		d=a-b;
+		System.out.println("Sub is "+d);
+		e=a*b;
+		System.out.println("Mul is "+e);
+		f=a/b;
+		System.out.println("Div is "+f);
+	}
+}
